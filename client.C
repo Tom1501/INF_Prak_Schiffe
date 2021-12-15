b39e2a4e3b8375478bd201438aab2320c8861375
MirkoBorcherding <mirko.borcherding@fh-bielefeld.de>
2021-12-15 18:39:08 +0100

    Stand Praktikumstermin

--- client.C
+++ client.C
@@ -20,24 +20,6 @@ int main() {
 	string msg;
 
 	//connect to host
-	c.conn(host , 2022);
-
-	int i=0;
-	bool goOn=1;
-	while(goOn){ // send and receive data
-		if((rand() % 20) < i++){
-			msg = string("BYEBYE");
-			goOn = 0;
-		}else{
-			msg = string("client wants this");
-		}
-		cout << "client sends:" << msg << endl;
-		c.sendData(msg);
-		msg = c.receive(32);
-		cout << "got response:" << msg << endl;
-		sleep(1);
-
-	}
+	c.conn(host , 2016);
 }
-
 
--- server.C
+++ server.C
@@ -18,33 +18,39 @@
 
 #include <iostream>
 #include "SIMPLESOCKET.H"
+#include "TASK3.H"
 
 class MyServer:public TCPserver{
 public:
 	MyServer(int portNMB,int maxSizeData):TCPserver(portNMB,maxSizeData){;};
 protected:
 	string myResponse(string input);
+	TASK3::World w;
+	TASK3::ShootResult res;
 };
 
 int main(){
 	srand(time(nullptr));
-	TCPserver srv(2022,25);
+	MyServer srv(2016,25);
 	srv.run();
-}
 
+
+}
 
 
 
 string MyServer::myResponse(string input){
 	int a,b,c;
 	string responseStr = string("0");
-	string responseBye = string("");
+	string responseBye = string("0");
+	string responseNew = string("0");
+	string responseErr = string("0");
 
 	if(input.compare(0,4,"cmd(") == 0){
 		sscanf(input.c_str() , "cmd(%i,%i,%i)", &a, &b, &c );
 		std::cout << " a=" << a << " b=" << b << " c=" << c << std::endl;
 
-		responseStr = string("OK");
+		responseStr = string("OK...");
 		return responseStr;
 
 	}
@@ -54,17 +60,34 @@ string MyServer::myResponse(string input){
 	if(input.compare(0,6,"shoot(") == 0){
 		sscanf(input.c_str() , " shoot(%i,%i)", &X, &Y );
 		std::cout << " X=" << X << " Y=" << Y << std::endl;
+			return responseStr;
+
 	}
 		else if(input.compare(0,9,"new_game(") == 0){
 			sscanf(input.c_str() , " new_game(");
+			TASK3::World w;
+			TASK3::ShootResult res;
+
+				do{
+					w.printBoard();
+					return "Eingabe: X=(Zahl)";
+					sscanf(input.c_str() , "X=(%i)", &X);
+					return "Eingabe: Y=(Zahl)";
+					sscanf(input.c_str() , "Y=(%i)", &Y);
+					cout << X << Y << endl;
+					res = w.shoot(X,Y);
+					cout << "shoot: (" << X << ", " << Y << ") --> " << res << endl;
+				}while(res != TASK3::GAME_OVER);
+				w.printBoard();
+			return responseNew;
 		}
-		else if(input.compare(0,9,"end_Game(") == 0){
-			sscanf(input.c_str() , " end_Game(");
+		else if(input.compare(0,9,"end_game(") == 0){
+			sscanf(input.c_str() , " end_game(");
 			responseBye = string("BYEBYE");
 			return responseBye;
 		}
 		else{
-			string responseStr = string("ERROR");
-			return responseStr;
+			string responseErr = string("ERROR..");
+			return responseErr;
 		}
 }
